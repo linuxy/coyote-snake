970bc50c8633d8eb06eeeb42204dc5261e603bb9
Ian Applegate <ian@jedi.sh>
2022-11-15 01:36:09 -0600

    ecs refactor

--- src/coyote-snake.zig
+++ src/coyote-snake.zig
@@ -19,7 +19,7 @@ const TILE_WIDTH = 32;
 const TILE_HEIGHT = 32;
 
 pub fn main() !void {
-    var world = World.create();
+    var world = try World.create();
     defer world.deinit();
 
     var game = try Game.init(world);
@@ -27,8 +27,8 @@ pub fn main() !void {
 
     while(game.isRunning) {
         game.handleEvents();
-        Systems.run(Update, .{world, game});
-        Systems.run(Render, .{world, game});
+        try Systems.run(Update, .{world, game});
+        try Systems.run(Render, .{world, game});
         c.SDL_Delay(17);
     }
 }
@@ -37,7 +37,6 @@ pub const Game = struct {
     world: *World,
     player: *Entity,
     tileMap: *Entity,
-    tails: std.ArrayList(*Entity),
 
     window: ?*c.SDL_Window,
     renderer: ?*c.SDL_Renderer,
@@ -66,6 +65,7 @@ pub const Game = struct {
             return error.SDLInitializationFailed;
         };
 
+        //One entity, many components
         self.player = try world.entities.create();
         self.tileMap = try world.entities.create();
 
@@ -73,17 +73,32 @@ pub const Game = struct {
         try self.player.attach(playerPosition, Components.Position{.x = 100, .y = 100});
 
         var playerTexture = try world.components.create(Components.Texture{});
-        try self.player.attach(playerTexture, Components.Texture{.id = "snake_head", .path = "assets/images/snake_head.bmp"});
+        try self.player.attach(playerTexture, Components.Texture{.id = "snake_head", .path = "assets/images/snake_head.bmp", .resource = try loadTexture(self, "assets/images/snake_head.bmp")});
 
-        self.tails = std.ArrayList(*Entity).init(allocator);
+        //One component, many entities
+        var tailTexture = try world.components.create(Components.Texture{});
+        var tailResource = try loadTexture(self, "assets/images/snake_body.bmp");
 
+        //Many entities, many components
         var i: usize = 0;
         while(i < START_SIZE) : (i += 1) {
             var tail = try world.entities.create();
-            //try self.tails.append(tail);
-            _ = tail;
+            var comp = try world.components.create(Components.Tail{});
+            try tail.attach(comp, Components.Tail{.x = 0.0, .y = 0.0});
+            try tail.attach(tailTexture, Components.Texture{.id = "snake_body", .path = "assets/images/snake_body.bmp", .resource = tailResource});
+            //std.log.info("setup tail: {*} id: {} comp id: {} comp type: {}", .{self.tails.items[i], self.tails.items[i].id, tailTexture.id, tailTexture.typeId.?});
         }
 
+        //Create tiles
+        var cur_x: c_int = 0;
+        var cur_y: c_int = 0;
+        var grassResource = try loadTexture(self, "assets/images/grass.bmp");
+        while(cur_y < SCREEN_HEIGHT) : (cur_y += TILE_HEIGHT) {
+            while(cur_x < SCREEN_WIDTH) : (cur_x += TILE_WIDTH) {
+                try addTile(world, "grass", "assets/images/grass.bmp", grassResource, cur_x, cur_y);
+            }
+            cur_x = 0;
+        }
         self.isRunning = true;
 
         return self;
@@ -123,8 +138,9 @@ pub const Components = struct {
     };
 
     pub const Tail = struct {
-        next: ?*Tail = null,
-        last: ?*Tail = null,
+        x: f64 = 0.0,
+        y: f64 = 0.0,
+        path: std.ArrayList(@Vector(2, f64)) = std.ArrayList(@Vector(2, f64)).init(allocator),
     };
 
     pub const Texture = struct {
@@ -133,9 +149,11 @@ pub const Components = struct {
         resource: ?*c.SDL_Texture = null,
     };
 
+    //Composition
     pub const Tile = struct {
-        width: u32,
-        height: u32,
+        x: c_int = 0,
+        y: c_int = 0,
+        texture: Texture = Texture{},
     };
 
     pub const Time = struct {
@@ -154,10 +172,35 @@ pub const Direction = enum {
 
 //Systems
 pub fn Render(world: *World, game: *Game) !void {
-    //Render TileMap entities
-    _ = world;
-    _ = game;
-    //Render Player and Tail entities
+
+    _ = c.SDL_RenderClear(game.renderer);
+    _ = c.SDL_SetRenderDrawColor(game.renderer, 255, 255, 255, 255);
+    
+    //Render tileMap
+    var it = world.components.iteratorFilter(Components.Tile{});
+    var i: usize = 0;
+    while(it.next()) |component| : (i += 1) {
+        var data = Cast(Components.Tile).get(component).?;
+        try renderToTile(game, data.texture.resource, data.x, data.y);
+    }
+
+    var player = game.player;
+
+    //Get one
+    var texture = Cast(Components.Texture).get(player.getByComponent(Components.Texture{}).?).?;
+    var position = Cast(Components.Position).get(player.getByComponent(Components.Position{}).?).?;
+
+    //Render Player
+    try renderToTile(game, texture.resource, @floatToInt(c_int, @round(position.x)), @floatToInt(c_int, @round(position.y)));
+
+    //Render Tails
+    var tails = world.entities.iteratorFilter(Components.Tail{});
+    while(tails.next()) |tail| {
+        var tailTexture = Cast(Components.Texture).get(tail.getByComponent(Components.Texture{}).?).?;
+        var tailPosition = Cast(Components.Tail).get(tail.getByComponent(Components.Tail{}).?).?;
+        try renderToTile(game, tailTexture.resource, @floatToInt(c_int, @round(tailPosition.x)), @floatToInt(c_int, @round(tailPosition.y)));
+    }
+    c.SDL_RenderPresent(game.renderer);
 }
 
 pub fn Update(world: *World, game: *Game) !void {
@@ -165,6 +208,20 @@ pub fn Update(world: *World, game: *Game) !void {
     _ = world;
     _ = game;
     //Update tail entities
+}
+
+pub inline fn addTile(world: *World, id: []const u8, path: []const u8, texture: ?*c.SDL_Texture, x: c_int, y: c_int) !void {
+    var tile = try world.entities.create();
+    var comp = try world.components.create(Components.Tile{});
+    try tile.attach(comp, Components.Tile{
+        .x = x,
+        .y = y,
+        .texture = Components.Texture{
+            .id = id,
+            .path = path,
+            .resource = texture,
+        },
+    });
 }
 
 pub inline fn renderToTile(game: *Game, texture: ?*c.SDL_Texture, x: c_int, y: c_int) !void {
@@ -185,4 +242,22 @@ pub inline fn renderToTile(game: *Game, texture: ?*c.SDL_Texture, x: c_int, y: c
         c.SDL_Log("Unable to render copy: %s", c.SDL_GetError());
         return error.SDL_RenderCopyExFailed;
     }
+}
+
+pub inline fn loadTexture(game: *Game, path: []const u8) !?*c.SDL_Texture {
+    var tempSurface = c.SDL_LoadBMP(@ptrCast([*c]const u8, path)) orelse
+    {
+        c.SDL_Log("Unable to load image: %s", c.SDL_GetError());
+        return error.SDL_IMG_LoadFailed;
+    };
+
+    var texture = c.SDL_CreateTextureFromSurface(game.renderer, tempSurface) orelse
+    {
+        c.SDL_Log("Unable to create surface: %s", c.SDL_GetError());
+        return error.SDL_CreateTextureFromSurfaceFailed;
+    };
+
+    c.SDL_FreeSurface(tempSurface);
+
+    return texture;
 }
