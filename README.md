0d115ff13333df92740d76b0fa35929fd2abe402
Ian Applegate <ian@jedi.sh>
2022-11-20 21:15:22 -0600

    mimalloc changes

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # coyote-snake
 An example game built with [coyote-ecs](https://github.com/linuxy/coyote-ecs), a fast and simple zig native ECS.
 
+To build:
+zig build mimalloc
+zig build
+
 ![Snake](<https://github.com/linuxy/coyote-snake/blob/main/assets/snake.gif> "snake!")
--- build.zig
+++ build.zig
@@ -5,6 +5,8 @@ pub fn build(b: *std.build.Builder) void {
 
     const ecsPkg = std.build.Pkg{ .name = "coyote-ecs", .source = std.build.FileSource{ .path = "vendor/coyote-ecs/src/coyote.zig" }};
 
+    const mimalloc = build_mimalloc(b);
+    
     const exe = b.addExecutable("snake", "src/coyote-snake.zig");
     exe.setBuildMode(mode);
     exe.linkLibC();
@@ -16,6 +18,8 @@ pub fn build(b: *std.build.Builder) void {
     exe.addLibraryPath("/opt/homebrew/Cellar/sdl2/2.24.2/lib");
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("SDL2_image");
+    exe.addLibraryPath("vendor/coyote-ecs/vendor/mimalloc");
+    exe.linkSystemLibrary("mimalloc");
     exe.addPackage(ecsPkg);
     //exe.use_stage1 = true;
     exe.install();
@@ -23,6 +27,30 @@ pub fn build(b: *std.build.Builder) void {
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
 
+    const make_step = b.step("mimalloc", "Make mimalloc library");
+    make_step.dependOn(&mimalloc.step);
+
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+}
+
+fn build_mimalloc(b: *std.build.Builder) *std.build.RunStep {
+
+    const cmake = b.addSystemCommand(
+        &[_][]const u8{
+            "cmake",
+            "-S./vendor/coyote-ecs/vendor/mimalloc/",
+            "-B./vendor/coyote-ecs/vendor/mimalloc/",
+        },
+    );
+    const make = b.addSystemCommand(
+        &[_][]const u8{
+            "make",
+            "-j4",
+            "-C./vendor/coyote-ecs/vendor/mimalloc",
+        },
+    );
+
+    make.step.dependOn(&cmake.step);
+    return make;
 }
