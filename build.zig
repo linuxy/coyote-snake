c45dacf0ad919af288001b9073c7ef6200159381
Ian Applegate <ian@jedi.sh>
2023-03-20 18:49:10 -0400

    update coyote-ecs and to 0.11.0 package+

--- build.zig
+++ build.zig
@@ -1,15 +1,22 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const ecsPkg = std.build.Pkg{ .name = "coyote-ecs", .source = std.build.FileSource{ .path = "vendor/coyote-ecs/src/coyote.zig" }};
+    const exe = b.addExecutable(.{
+        .root_source_file = .{ .path = "src/coyote-snake.zig"},
+        .optimize = optimize,
+        .target = target,
+        .name = "snake",
+    });
 
-    const mimalloc = build_mimalloc(b);
-    
-    const exe = b.addExecutable("snake", "src/coyote-snake.zig");
-    exe.setBuildMode(mode);
     exe.linkLibC();
+
+    exe.addAnonymousModule("coyote-ecs", .{ 
+        .source_file = .{ .path = "./vendor/coyote-ecs/src/coyote.zig" },
+    });
+
     //Linux paths
     exe.addIncludePath("/usr/include");
     exe.addIncludePath("/usr/include/x86_64-linux-gnu");
@@ -18,39 +25,11 @@ pub fn build(b: *std.build.Builder) void {
     exe.addLibraryPath("/opt/homebrew/Cellar/sdl2/2.24.2/lib");
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("SDL2_image");
-    exe.addLibraryPath("vendor/coyote-ecs/vendor/mimalloc");
-    exe.linkSystemLibrary("mimalloc");
-    exe.addPackage(ecsPkg);
-    //exe.use_stage1 = true;
     exe.install();
 
     const run_cmd = exe.run();
     run_cmd.step.dependOn(b.getInstallStep());
 
-    const make_step = b.step("mimalloc", "Make mimalloc library");
-    make_step.dependOn(&mimalloc.step);
-
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-}
-
-fn build_mimalloc(b: *std.build.Builder) *std.build.RunStep {
-
-    const cmake = b.addSystemCommand(
-        &[_][]const u8{
-            "cmake",
-            "-S./vendor/coyote-ecs/vendor/mimalloc/",
-            "-B./vendor/coyote-ecs/vendor/mimalloc/",
-        },
-    );
-    const make = b.addSystemCommand(
-        &[_][]const u8{
-            "make",
-            "-j4",
-            "-C./vendor/coyote-ecs/vendor/mimalloc",
-        },
-    );
-
-    make.step.dependOn(&cmake.step);
-    return make;
 }
